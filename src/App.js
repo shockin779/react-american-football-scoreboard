049638c8fdc8f466ea55323065f0fe421a61332a
Sean Hockin <sean.hockin@gmail.com>
2019-09-17 23:05:56 -0600

    Quarter button implemented and reset game function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+  const [quarterButtonText, setQuarterText] = useState('Next Quarter');
+
+  function resetGame() {
+    setQuarter(1);
+    setHomeScore(0);
+    setAwayScore(0);
+  }
 
   return (
     <div className="container">
@@ -26,13 +34,22 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+        </div>
+        <div>
+          <button className='quarterButton' onClick={() => {
+            if(quarter < 4) {
+              setQuarter(quarter + 1);
+            } else {
+              resetGame();
+            }
+          }}>{quarterButtonText}</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
